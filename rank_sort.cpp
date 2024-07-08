f4da80bbcb450ad4731de97a862db2d35b04642d
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2024-07-08 00:44:19 -0500

    feat(util): Add `check_sorted`

--- rank_sort.cpp
+++ rank_sort.cpp
@@ -5,6 +5,18 @@
 
 #include <mpi.h>
 
+bool check_sorted(int* arr, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(int argc, char** argv)
 {
     MPI_Init(&argc, &argv);
